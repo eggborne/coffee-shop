01b872031fce817f42a5a82a5cf8aa4c1662a6e2
Mike Donovan <mike@mikedonovan.dev>
2023-04-21 16:26:56 -0700

    Establish basic framework

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer>
+      <div>made for Epicodus by mike@mikedonovan.dev</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header>
+      <h1>React Coffee Shop</h1>
+    </header>
+  );
+}
+
+export default Header;
